0c22750a9bdee6b5fa7b60ed2f4a449a8a4ea137
Rodrigo Torres <rodrigo.gviva@gmail.com>
2022-05-04 09:51:23 -0300

    primeira aula concluida

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
+import { Widget } from "./components/Widget"
 
-interface ButtonProps{
-  text?: string;
-}
 
-function Button (props: ButtonProps){
-  return <button className="bg-violet-500 p-2 h-10 rounded">{props.text ?? 'Default'}</button>
-}
-
-function App() {
+export function App() {
   return (
-    <div className="flex gap-2">
-      <Button text="Enviar"/>
-      <Button />
-    </div>
+    <Widget />
   )
 }
 
-export default App
