771a2875f0b612e5f66f19ea3855f5231f16637b
Rodrigo Torres <rodrigo.gviva@gmail.com>
2022-06-24 09:21:05 -0300

    definir porta

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,14 +5,12 @@ import { routes } from './routes';
 
 const app = express();
 
-const PORT = 3333;
-
 app.use(cors())
 
 app.use(express.json());
 
 app.use(routes)
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || 3333, ()=>{
   console.log(`HTTP server running on port ${PORT}`)
 });
